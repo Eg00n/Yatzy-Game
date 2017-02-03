73fdde030873e22862029dbd5c5939834327af6d
Jakob Jespersen <jespersen92@gmail.com>
2017-02-03 13:20:10 +0100

    Added function to keep rolled Dice

--- yatzyJavascript.js
+++ yatzyJavascript.js
@@ -18,32 +18,39 @@
 				var die4 = document.getElementById("die4");
 				var die5 = document.getElementById("die5");
 				
-				var d1 = Math.floor(Math.random() * 6) + 1;
-				var d2 = Math.floor(Math.random()*6)+1;
-				var d3 = Math.floor(Math.random()*6)+1;
-				var d4 = Math.floor(Math.random()*6)+1;
-				var d5 = Math.floor(Math.random()*6)+1;
-				
-				die1.innerHTML = d1;
-				die2.innerHTML = d2;
-				die3.innerHTML = d3;
-				die4.innerHTML = d4;
-				die5.innerHTML = d5;
+				if(die1.className != "CheckedDie") {
+					var d1 = Math.floor(Math.random() * 6) + 1;
+					die1.innerHTML = d1;
+				}
+				if(die2.className != "CheckedDie") {
+					var d2 = Math.floor(Math.random() * 6) + 1;
+					die2.innerHTML = d2;
+				}
+				if(die3.className != "CheckedDie") {
+					var d3 = Math.floor(Math.random() * 6) + 1;
+					die3.innerHTML = d3;
+				}
+				if(die4.className != "CheckedDie") {
+					var d4 = Math.floor(Math.random() * 6) + 1;
+					die4.innerHTML = d4;
+				}
+				if(die5.className != "CheckedDie") {
+					var d5 = Math.floor(Math.random() * 6) + 1;
+					die5.innerHTML = d5;
+				}
 			}
 			
-			var timeToShow = 0;
+			/*var timeToShow = 0;
 			
 			function display() {
 				timeToShow++;
 				document.getElementById("time").innerHTML = timeToShow;
 				window.setTimeout(display, 1000);
-			}
+			}*/
 			
-			function Check(){
-				var die = document.getElementByClassName("Dice");			
-				
-				if(die.checked==false)
-				{die.checked = true;}
-				if(die.checked==true)
-				{die.checked = false;}
+			function checkDie(id){
+				if(document.getElementById(id).className == "CheckedDie")
+					document.getElementById(id).className = "UnCheckedDie";
+				else if(document.getElementById(id).className == "UnCheckedDie")
+					document.getElementById(id).className = "CheckedDie";
 			}
