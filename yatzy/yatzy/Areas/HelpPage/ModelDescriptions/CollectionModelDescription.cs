e2884f6e55a549c2e8e3c3cc8cb224830ace8762
Jakob Jespersen <jespersen92@gmail.com>
2017-03-05 18:08:31 +0100

    Added VS project Started to change javascript to C#

---
+++ yatzy/yatzy/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace yatzy.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ yatzy/yatzy/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace yatzy.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
